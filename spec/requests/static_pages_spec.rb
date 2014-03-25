c007740e80d1f52e6078b3e77d1957211684583f
Juan Ruiz <invitado@perdidita.(none)>
2014-03-25 20:42:38 +0000

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -41,13 +41,13 @@ describe "Static pages" do
   describe "Help page" do
     before { visit help_path }
     it { should have_content('Help') }
-    it { should have_title("Ruby on Rails Tutorial Sample App | Help") }
+    it { should have_title(full_title('Help'))}
   end
 
   describe "About page" do
     before { visit about_path }
     it { should have_content('About Us') }
-    it { should have_title("Ruby on Rails Tutorial Sample App | About Us") }
+    it { should have_title(full_title('About Us'))}
   end
 
 
