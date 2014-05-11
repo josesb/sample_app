2c512c22eb1b26981b211de28210840e0e1e91e8
josesb <josesb@hotmail.com>
2014-05-11 02:20:33 +0100

    Finish static pages - Contact

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -28,7 +28,6 @@ describe "Static pages" do   #describe las paginas estaticas
     it { should_not have_title('| Home') }
   end
 
-
   describe "Help page" do
     before { visit help_path }  #pag 50 DAN 13
     it { should have_content('Help')}
@@ -38,7 +37,6 @@ describe "Static pages" do   #describe las paginas estaticas
 
   end
 
-
   describe "About page" do
     before { visit about_path }  #pag 50 DAN 13
     it { should have_content('About')}
@@ -46,6 +44,15 @@ describe "Static pages" do   #describe las paginas estaticas
     #it { should have_title("Ruby on Rails Tutorial Sample App") }
     #it { should_not have_title('| About') }
   end
+
+  describe "Contact page" do
+    before { visit contact_path }  #pag 50 DAN 13
+    it { should have_content('Contact')}
+    it { should have_title(full_title('')) }
+    #it { should have_title("Ruby on Rails Tutorial Sample App") }
+    #it { should_not have_title('| Contact') }
+  end
+
 
 end
 
